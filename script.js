9723063b1a70cc52efc0fe3d7556a0146356b3e6
ewelina <ewelina.brzozowiec@wp.pl>
2019-01-01 17:35:27 +0100

    Add new files

---
+++ script.js
@@ -0,0 +1,13 @@
+const ol = document.querySelectorAll('ol');
+const li = document.querySelectorAll('li');
+const  ad1 = document.querySelector('[value="dwa"]').checked;
+const input = document.querySelector('.dwa')
+let licznik = 0;
+if(ad1 == true){
+
+    licznik++;
+    console.log (licznik);
+
+} else {
+    console.log("jest oki")
+}
